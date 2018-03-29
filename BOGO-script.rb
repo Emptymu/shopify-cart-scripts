d697b9e4dc5ab2e974cc436c21abccf8636d6dd3
Jasper Wang <jasper920011@gmail.com>
2018-03-29 13:56:28 -0700

    Update LowToHightPartioner

--- BOGO-script.rb
+++ BOGO-script.rb
@@ -28,11 +28,11 @@ class PercentageDiscount
 end
 
 class LowToHighPartitioner
-
   def initialize(paid_item_count, discounted_item_count)
     @paid_item_count = paid_item_count
     @discounted_item_count = discounted_item_count
   end
+
   def partition(cart, applicable_line_items)
     # Sort the items by price from low to high
     sorted_items = applicable_line_items.sort_by{|line_item| line_item.variant.price}
@@ -40,20 +40,31 @@ class LowToHighPartitioner
     total_applicable_quantity = sorted_items.map(&:quantity).reduce(0, :+)
     # Find the quantity of items that must be discounted
     discounted_items_remaining = Integer(total_applicable_quantity / (@paid_item_count + @discounted_item_count) * @discounted_item_count)
+    puts discounted_items_remaining
     # Create an array of items to return
     discounted_items = []
 
+    # Loop over all the items and find those to be discounted
     sorted_items.each do |line_item|
+      # Exit the loop if all discounted items have been found
       break if discounted_items_remaining == 0
+      # The item will be discounted
       discounted_item = line_item
       if line_item.quantity > discounted_items_remaining
+        # If the item has more quantity than what must be discounted, split it
         discounted_item = line_item.split(take: discounted_items_remaining)
+
+        # Insert the newly-created item in the cart, right after the original item
         position = cart.line_items.find_index(line_item)
         cart.line_items.insert(position + 1, discounted_item)
       end
+      # Decrement the items left to be discounted
       discounted_items_remaining -= discounted_item.quantity
+      # Add the item to be returned
       discounted_items.push(discounted_item)
     end
+  # Example
+  # ------- Check to see if additional discount code is used. If used, add customer discount code and remove BOGO. Same applies if discount code is removed. Apply BOGO instead.
         cart_discounted_subtotal =
       case cart.discount_code
       when CartDiscount::Percentage
@@ -71,6 +82,8 @@ class LowToHighPartitioner
       else
         cart.subtotal_price
       end
+    #--- 
+    # Return the items to be discounted
     discounted_items
   end
 end
