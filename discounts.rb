d0fe7778dfd2728e671b3be417e6ddf8ecd77c70
Jasper Wang <jasper920011@gmail.com>
2018-04-18 10:01:28 -0700

    Add fix amount discount per product

--- discounts.rb
+++ discounts.rb
@@ -52,3 +52,33 @@ class PercentageDiscountPerProduct
     puts "#{line_discount} off for #{count} products!"
   end
 end
+
+class FixAmountDiscountPerProduct
+  def initialize(amount, message)
+    # Calculate the percentage, while ensuring that Decimal values are used in
+    # order to maintain precision.
+    @amount = Money.new(cents: amount)
+    @message = message
+  end
+
+  def apply(item_info)
+    # line item to apply discount
+    line_item = item_info['item'];
+    # how many products to apply discount in this line item
+    count = item_info['count'];
+    
+    # Calculate the discount for this line item
+    line_discount = @amount * count
+
+    # Calculated the discounted line price
+    new_line_price = line_item.line_price - line_discount
+
+    # Apply the new line price to this line item with a given message
+    # describing the discount, which may be displayed in cart pages and
+    # confirmation emails to describe the applied discount.
+    line_item.change_line_price(new_line_price, message: @message)
+
+    # Print a debugging line to the console
+    puts "#{line_discount} off for #{count} products!"
+  end
+end
