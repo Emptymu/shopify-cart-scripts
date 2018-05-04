56c57e927f5e4c4c04975716a2de5efbd9ac5851
Jasper Wang <jasper920011@gmail.com>
2018-05-04 11:12:13 -0700

    Add percentage discount

--- buy_one_get_another_one.rb
+++ buy_one_get_another_one.rb
@@ -12,6 +12,31 @@ class ListHasItemSelector
   end
 end
 
+class PercentageDiscount
+  def initialize(percent, message)
+    # Calculate the percentage, while ensuring that Decimal values are used in
+    # order to maintain precision.
+    @percent = Decimal.new(percent) / 100.0
+    @message = message
+  end
+
+  def apply(line_item)
+    # Calculate the discount for this line item
+    line_discount = line_item.line_price * @percent
+
+    # Calculated the discounted line price
+    new_line_price = line_item.line_price - line_discount
+
+    # Apply the new line price to this line item with a given message
+    # describing the discount, which may be displayed in cart pages and
+    # confirmation emails to describe the applied discount.
+    line_item.change_line_price(new_line_price, message: @message)
+
+    # Print a debugging line to the console
+    puts "Discounted line item with variant #{line_item.variant.id} by #{line_discount}."
+  end
+end
+
 class BuyOneGetAnotherPartitioner
   def initialize(paid_item_count, discounted_item_count)
     @paid_item_count = paid_item_count
